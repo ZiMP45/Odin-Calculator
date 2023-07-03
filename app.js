916036dccb92c172525beaaed3eb3e55fe3f4a4f
Zak Porche <93185579+ZiMP45@users.noreply.github.com>
2023-07-03 01:30:02 +0000

    get value from button press, add to output panel

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// add event listeners to buttons
 // on click, push that button value to an array for numbers
 // add functions to operator buttons
 // leave off button alone for now
 // on click, whatever button is pressed add to output box
 
 const buttons = document.querySelectorAll('button');
+const output = document.querySelector('.output');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -21,5 +21,14 @@ buttons.forEach((button) => {
             button.style.transform = "scale(1.0)";
             button.style.border = "none";
         }
+
+        let value = button.textContent;
+
+        updateOutput(value);
+
     })
 })
+
+function updateOutput(value) {
+    output.textContent = value;
+}
